293157289ee2ce5fdba77555b15e8d7febb1653d
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-06-10 13:05:03 -0300

    [UPDATE] Route

--- src/code/codeController.js
+++ src/code/codeController.js
@@ -4,7 +4,7 @@ const Code = mongoose.model('code');
 
 module.exports = {
 	async indexByCode(req, res) {
-		const existente = await Code.findOne({ code: req.query.code });
+		const existente = await Code.findOne({ code: req.params.code });
 
 		if (existente) {
 			res.send(existente);
